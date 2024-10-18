caa648313baf2dbc2eed9bb0f04f2f96a9aa9a0c
redig <luizredig@gmail.com>
2024-10-18 00:55:10 -0300

    feat(sidebar): add new items and improve sidebar styles

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,41 @@
+import { AppWindowIcon, CogIcon, ListIcon } from "lucide-react";
 import { Card, CardContent, CardHeader } from "../ui/card";
+import SidebarItem from "./SidebarItem";
 
 const Sidebar = () => {
   return (
     <>
-      <Card className="h-100 flex w-96 flex-col border-none p-0">
-        <CardHeader className="flex h-14 flex-row items-center justify-between bg-primary p-0">
-          <img src="/logo.svg" alt="Logo" className="ml-5 h-10 w-10" />
+      {/* Sidebar */}
+      <Card className="h-100 flex w-80 flex-col border-none p-0">
+        {/* Header */}
+        <CardHeader className="flex h-12 flex-row items-center justify-between bg-primary p-0">
+          <img src="/logo.svg" alt="Logo" className="ml-5 h-6 w-6" />
         </CardHeader>
 
-        <CardContent className="flex flex-1 bg-secondary"></CardContent>
+        {/* Content */}
+        <CardContent className="flex flex-1 flex-col bg-secondary p-0">
+          {/* Sidebar Button Item */}
+          <SidebarItem
+            type="button"
+            title="Painel de sugestões"
+            icon={<AppWindowIcon className="text-primary" />}
+            path="/painel-de-sugestoes"
+          />
+
+          {/* Sidebar Accordion Item - Sugestões */}
+          <SidebarItem
+            type="accordion"
+            title="Sugestões"
+            icon={<ListIcon className="text-primary" />}
+          >
+            <SidebarItem
+              type="button"
+              title="Gerenciar"
+              icon={<CogIcon className="text-primary" size={20} />}
+              path="/painel-de-sugestoes/gerenciar"
+            />
+          </SidebarItem>
+        </CardContent>
       </Card>
     </>
   );
