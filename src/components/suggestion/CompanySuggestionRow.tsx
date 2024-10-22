4a292fba231fb48b6db9864abbfd80c1bcd9e2ee
redig <luizredig@gmail.com>
2024-10-22 15:36:52 -0300

    style(company-suggestion-row): improve horizontal scroll with ScrollArea component

--- src/components/suggestion/CompanySuggestionRow.tsx
+++ src/components/suggestion/CompanySuggestionRow.tsx
@@ -2,6 +2,7 @@ import { Separator } from "@/components/ui/separator";
 import { BuildingIcon } from "lucide-react";
 import SuggestionCard from "./SuggestionCard";
 import { Company, Suggestion } from "@prisma/client";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
 type CompanySuggestionRowProps = {
   company: Company;
@@ -13,7 +14,7 @@ const CompanySuggestionRow = ({
   suggestions,
 }: CompanySuggestionRowProps) => {
   return (
-    <div className="flex flex-col gap-4 py-5">
+    <div className="flex flex-col py-5">
       <div className="flex w-fit flex-col gap-4 px-5">
         <div className="flex items-center gap-2">
           <BuildingIcon className="h-8 w-8 text-primary" />
@@ -26,11 +27,15 @@ const CompanySuggestionRow = ({
         <Separator />
       </div>
 
-      <div className="flex flex-1 gap-10 overflow-x-scroll px-5">
-        {suggestions.map((suggestion: Suggestion) => (
-          <SuggestionCard key={suggestion.id} suggestion={suggestion} />
-        ))}
-      </div>
+      <ScrollArea className="flex flex-col py-4">
+        <div className="flex gap-10 px-5">
+          {suggestions.map((suggestion: Suggestion) => (
+            <SuggestionCard key={suggestion.id} suggestion={suggestion} />
+          ))}
+        </div>
+
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
--- src/pages/painel-de-sugestoes/SuggestionPanel.tsx
+++ src/pages/painel-de-sugestoes/SuggestionPanel.tsx
@@ -11,10 +11,12 @@ const SuggestionPanel = () => {
     <Page
       title="Painel de Sugestões"
       children={
-        <div className="flex flex-col">
-          <Search />
+        <div className="flex w-full flex-col">
+          <div className="z-10 bg-muted p-5 shadow">
+            <Search />
+          </div>
 
-          <div className="flex h-screen w-full flex-col gap-8 overflow-x-hidden">
+          <div className="flex h-screen w-full flex-col overflow-y-scroll">
             {companies.map((company) => (
               <CompanySuggestionRow
                 key={company.id}
