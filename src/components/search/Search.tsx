9956cc7d30b7a08fa40baae7c5db246f2d12f4db
redig <luizredig@gmail.com>
2024-10-22 16:00:17 -0300

    feat(search): begin process of converting search component into a submittable form using shadcn Form

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,28 +1,228 @@
-import DatePicker from "../datepicker/Datepicker";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
 import { Card } from "../ui/card";
 import { Input } from "../ui/input";
-import { Select } from "../ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Calendar } from "../ui/calendar";
+import { Separator } from "../ui/separator";
+
+const FormSchema = z.object({
+  search: z.string().optional(),
+  startDate: z.date().optional(),
+  endDate: z.date().optional(),
+  company: z.string().optional(),
+  status: z.string().optional(),
+});
 
 const Search = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <Card>
-        {/* Buscar */}
-        <Input />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <Card className="flex flex-col gap-4 p-5">
+          <div className="flex flex-col gap-2">
+            <h2 className="font-semibold text-primary">Filtro de busca</h2>
 
-        {/* Data de início */}
-        <DatePicker />
+            <Separator />
+          </div>
 
-        {/* Data de fim */}
-        <DatePicker />
+          <div className="flex items-end gap-3">
+            {/* Buscar */}
+            <FormField
+              name="search"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full">
+                  <FormControl>
+                    <Input placeholder="Buscar..." {...field} />
+                  </FormControl>
 
-        {/* Select de Empresa */}
-        <Select />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-        {/* Select de Status */}
-        <Select />
-      </Card>
-    </>
+            {/* Data de Início */}
+            <FormField
+              name="startDate"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full flex-col">
+                  <FormLabel>Data de Início</FormLabel>
+
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className="pl-3 text-left font-normal"
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Escolha a data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date > new Date()}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Data de Fim */}
+            <FormField
+              name="endDate"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full flex-col">
+                  <FormLabel>Data de Fim</FormLabel>
+
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          className="pl-3 text-left font-normal"
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Escolha a data</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) => date > new Date()}
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Select de Empresa */}
+            <FormField
+              name="company"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full flex-col">
+                  <FormLabel>Empresa</FormLabel>
+                  <FormControl>
+                    <Select {...field}>
+                      <SelectTrigger>
+                        <SelectValue />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="ravibras">Ravibras</SelectItem>
+
+                          <SelectItem value="automatech">Automatech</SelectItem>
+
+                          <SelectItem value="newblow">Newblow</SelectItem>
+
+                          <SelectItem value="unipoli">Unipoli</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Select de Status */}
+            <FormField
+              name="status"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex w-full flex-col">
+                  <FormLabel>Status</FormLabel>
+
+                  <FormControl>
+                    <Select {...field}>
+                      <SelectTrigger className="flex w-full">
+                        <SelectValue />
+                      </SelectTrigger>
+
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectItem value="novo">Novo</SelectItem>
+
+                          <SelectItem value="emAndamento">
+                            Em andamento
+                          </SelectItem>
+
+                          <SelectItem value="concluido">Concluído</SelectItem>
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="hover:bg-primary">
+              Buscar
+            </Button>
+          </div>
+        </Card>
+      </form>
+    </Form>
   );
 };
 
