6650a3ac3beccf88f3bf6fd540e06f52ee9100f6
redig <luizredig@gmail.com>
2024-10-28 13:38:42 -0300

    style(form): improve suggestion form styles in user view

--- src/pages/template/Page.tsx
+++ src/pages/template/Page.tsx
@@ -8,10 +8,10 @@ export type PageProps = {
 const Page = ({ title, children }: PageProps) => {
   return (
     <>
-      <div className="flex w-full flex-col overflow-x-hidden overflow-y-hidden">
+      <div className="flex h-full w-full flex-col overflow-x-hidden overflow-y-hidden">
         <Topbar title={title} />
 
-        <div className="flex overflow-scroll [&::-webkit-scrollbar]:hidden">
+        <div className="flex h-full flex-1 overflow-scroll [&::-webkit-scrollbar]:hidden">
           {children}
         </div>
       </div>
